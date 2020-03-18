eca47d0327c99e7c7f6b340f624603b247f2d84b
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-03-18 21:36:40 +0800

    Wire up login form to API
    Connects the login form button to the Authentication API endpoint. (/token). Gets back the bearer toek or an exemption

---
+++ TRMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Configuration;
+using TRMDesktopUI.Models;
+
+namespace TRMDesktopUI.Helpers
+{
+	public class APIHelper : IAPIHelper
+	{
+		private HttpClient apiClient
+		{
+			get; set;
+		}
+
+		public APIHelper ()
+		{
+			InitializeClient();
+		}
+
+		private void InitializeClient ()
+		{
+			string api = ConfigurationManager.AppSettings["api"];
+
+			apiClient = new HttpClient();
+			apiClient.BaseAddress = new Uri(api);
+			apiClient.DefaultRequestHeaders.Accept.Clear();
+			apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+		}
+
+		public async Task<AuthenticatedUser> Authenticate (string username, string password)
+		{
+			// ranielgarcia101@gmail.com
+			// SWORDfish@2020
+			var data = new FormUrlEncodedContent(new []
+			{
+				new KeyValuePair<string, string>("grant_type", "password"),
+				new KeyValuePair<string, string>("username", username),
+				new KeyValuePair<string, string>("password", password)
+			});
+
+			using (HttpResponseMessage response = await apiClient.PostAsync("/Token", data))
+			{
+				if (response.IsSuccessStatusCode)
+				{
+					var results = await response.Content.ReadAsAsync<AuthenticatedUser>();
+					return results;
+				}
+				else
+				{
+					throw new Exception(response.ReasonPhrase);
+				}
+			}
+
+		}
+	}
+}
