c3e866e2c5f1dfcc19c5b3ab112fd58a3d14ad00
Raniel Garcia <ranielgarcia2596@gmail.com>
2020-04-11 21:26:27 +0800

    Last changes

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Net.Http;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
 using TRMDataManager.Library.Models;
+using TRMDataManager.Models;
 
 namespace TRMDataManager.Controllers
 {
@@ -23,5 +25,81 @@ namespace TRMDataManager.Controllers
             return data.GetUserById(userId).First();
         }
 
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers ()
+        {
+            List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var users = userManager.Users.ToList();
+                var roles = context.Roles.ToList();
+
+
+                foreach (var user in users)
+                {
+                    ApplicationUserModel u = new ApplicationUserModel
+                    {
+                        Id = user.Id,
+                        Email = user.Email
+                    };
+
+                    foreach (var r in user.Roles)
+                    {
+                        u.Roles.Add(r.RoleId, roles.Where(x => x.Id == r.RoleId).First().Name);
+                    }
+                    output.Add(u);
+                }
+            }
+
+            return output;
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("Admin/GetAllRoles")]
+        public Dictionary<string, string> GetAllRoles ()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roles = context.Roles.ToDictionary(x => x.Id, x => x.Name);
+                return roles;
+            }
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("Admin/AddRole")]
+        public void AddARole (UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.AddToRole(pairing.UserId, pairing.RoleName);
+            }
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("Admin/RemoveRole")]
+        public void RemoveARole (UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.RemoveFromRole(pairing.UserId, pairing.RoleName);
+            }
+        }
     }
 }
